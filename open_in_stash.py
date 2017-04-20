0411701f2bbeb74297538c486fd8ac7e9bc01299
Jingjing Duan <jduan@fitbit.com>
2017-04-20 10:26:30 -0700

    open_in_stash.py

---
+++ open_in_stash.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+# This script takes in a file like
+# /Users/jduan/workspace/path/to/file/in/git/repo
+# and opens it in stash.
+
+import os
+import sys
+import subprocess
+
+URL_TEMPLATE = "https://source.fitbit.com/projects/%s/repos/%s/browse/%s"
+
+def locate_git_repo(filepath):
+    while True:
+        parent_dir = os.path.dirname(filepath)
+        if parent_dir is filepath:
+            raise Exception("Failed to find a git repo!")
+        if os.path.exists(os.path.join(parent_dir, ".git")):
+            return parent_dir
+        filepath = parent_dir
+
+def find_filepath_from_repo_root(repo_dir, filepath):
+    return filepath[len(repo_dir) + 1:]
+
+def find_group_and_repo_name(dirname):
+    os.chdir(dirname)
+    output = subprocess.check_output("git config --get remote.origin.url", shell=True)
+    # output looks like: ssh://git@source.fitbit.com/group/repo_name.git
+    parts = output.rstrip().split("/")
+    group = parts[-2]
+    repo_name = parts[-1]
+    # remote the ".git" extension
+    repo_name = repo_name.split(".")[0]
+    return (group, repo_name)
+
+filepath = os.path.realpath(sys.argv[1])
+dirname = locate_git_repo(filepath)
+relative_path = find_filepath_from_repo_root(dirname, filepath)
+group, repo_name = find_group_and_repo_name(dirname)
+url = URL_TEMPLATE % (group, repo_name, relative_path)
+subprocess.call("echo '%s' | pbcopy" % url, shell=True)
+# print(url)
+# subprocess.call("open %s" % url, shell=True)
