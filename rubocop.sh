8f5f95497feb57e692a81bcd0ba9907ed42b24d1
Jingjing Duan <duanjingjing@gmail.com>
2018-07-30 15:46:22 -0700

    make rubocop consider Ruby files only

--- rubocop.sh
+++ rubocop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run "rubocop" on the files that have been changed on a user branch
-git diff origin/master...HEAD --name-only | xargs bundle exec rubocop
+git diff origin/master...HEAD --name-only | grep ".rb$" | xargs bundle exec rubocop
